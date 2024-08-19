16ba6c5d86b345ddae0814f34e87adbe52e063f0
dyf96 <yingfei.dai@atrae.co.jp>
2024-08-19 18:36:33 +0900

    feat: ✨ add sql command enum

---
+++ src/domain/enums/CommandTypesEnum.ts
@@ -0,0 +1,8 @@
+export const commandTypes = {
+  SELECT: "SELECT",
+  INSERT: "INSERT",
+  UPDATE: "UPDATE",
+  DELETE: "DELETE",
+} as const;
+
+export type CommandTypesEnum = (typeof commandTypes)[keyof typeof commandTypes];
