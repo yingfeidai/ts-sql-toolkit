82320532ff1af1039ac50a1939aa85227859c4fc
dyf96 <yingfei.dai@atrae.co.jp>
2024-08-21 18:39:13 +0900

    refactor: 🔨 remove not use code and move code to sql validator

--- src/domain/validators/QueryValidator.ts
+++ src/domain/validators/QueryValidator.ts
@@ -1,61 +1,66 @@
-import { QueryParams } from "../dtos/QueryParams";
-import { CountParams } from "../dtos/CountParams";
-import { BatchInsertParams } from "../dtos/BatchInsertParams";
-import { IFieldValidator } from "../interfaces/IFieldValidator";
-import { IOperatorValidator } from "../interfaces/IOperatorValidator";
-import { IQueryValidator } from "../interfaces/IQueryValidator";
-import { validateJoinOptions, validatePagination, validateOrderByOptions, validateWhereOptions, validateData } from "./ValidationHelpers";
-import { IValueValidator } from "../interfaces/IValueValidator";
+import { QueryParams } from '../dtos/QueryParams'
+import { CountParams } from '../dtos/CountParams'
+import { BatchInsertParams } from '../dtos/BatchInsertParams'
+import { IFieldValidator } from '../interfaces/IFieldValidator'
+import { IQueryValidator } from '../interfaces/IQueryValidator'
+import {
+  validateJoinOptions,
+  validatePagination,
+  validateOrderByOptions,
+  validateWhereOptions,
+  validateData,
+} from './ValidationHelpers'
+import { IValueValidator } from '../interfaces/IValueValidator'
 
-export class QueryValidator<FieldsEnum extends string> implements IQueryValidator<FieldsEnum> {
-
+export class QueryValidator<FieldsEnum extends string>
+  implements IQueryValidator<FieldsEnum> {
   constructor(
     private fieldValidator: IFieldValidator,
-    private operatorValidator: IOperatorValidator,
-    private valueValidator: IValueValidator
+    private valueValidator: IValueValidator,
   ) {}
 
   validateCount(params: CountParams<FieldsEnum>): void {
     if (params.select) {
-      this.validateFields([params.select]); 
+      this.validateFields([params.select])
     }
     if (params.where) {
-      validateWhereOptions(params.where, this.fieldValidator, this.operatorValidator, this.valueValidator); 
+      validateWhereOptions(
+        params.where,
+        this.fieldValidator,
+        this.valueValidator,
+      )
     }
     if (params.join) {
-      validateJoinOptions(params.join, this.fieldValidator); 
-    }
-  }
-  
-  validateSelect(params: QueryParams<FieldsEnum>): void {
-    if (params.select) {
-      this.validateFields(params.select); 
-    }
-    if (params.where) {
-      validateWhereOptions(params.where, this.fieldValidator, this.operatorValidator, this.valueValidator); 
-    }
-    if (params.pagination) {
-      validatePagination(params.pagination);
-    }
-    if (params.join) {
-      validateJoinOptions(params.join, this.fieldValidator); 
-    }
-    if (params.orderBy) {
-      validateOrderByOptions(params.orderBy, this.fieldValidator); 
-    }
-    if (params.groupBy) {
-      this.validateFields(params.groupBy); 
+      validateJoinOptions(params.join, this.fieldValidator)
     }
   }
 
-  validateBatchInsert(params: BatchInsertParams<FieldsEnum>): void {
-    if (params.batchSize <= 0) {
-      throw new Error(`Invalid batch size: ${params.batchSize}`);
+  validateSelect(params: QueryParams<FieldsEnum>): void {
+    if (params.select) {
+      this.validateFields(params.select)
     }
-    validateData(params.data, this.fieldValidator, this.valueValidator); 
+    if (params.where) {
+      validateWhereOptions(
+        params.where,
+        this.fieldValidator,
+        this.valueValidator,
+      )
+    }
+    if (params.pagination) {
+      validatePagination(params.pagination)
+    }
+    if (params.join) {
+      validateJoinOptions(params.join, this.fieldValidator)
+    }
+    if (params.orderBy) {
+      validateOrderByOptions(params.orderBy, this.fieldValidator)
+    }
+    if (params.groupBy) {
+      this.validateFields(params.groupBy)
+    }
   }
 
   private validateFields(fields: FieldsEnum[]): void {
-    this.fieldValidator.validateFields(fields);
+    this.fieldValidator.validateFields(fields)
   }
 }
--- src/domain/validators/ValidationHelpers.ts
+++ src/domain/validators/ValidationHelpers.ts
@@ -1,93 +1,71 @@
-import { joinTypes, JoinTypesEnum } from "../enums/JoinTypesEnum";
-import { orderByTypes, OrderByTypesEnum } from "../enums/OrderByTypesEnum";
-import { IFieldValidator } from "../interfaces/IFieldValidator";
-import { IOperatorValidator } from "../interfaces/IOperatorValidator";
-import { IValueValidator } from "../interfaces/IValueValidator";
-import { JoinOptionsType } from "../types/JoinOptionsType";
-import { OrderByOptionsType } from "../types/OrderByOptionsType";
-import { PaginationOptionsType } from "../types/PaginationOptionsType";
-import { PartialFieldValueMapType } from "../types/PartialFieldValueMapType";
-import { WhereOptionsType } from "../types/WhereOptionsType";
-
+import { IFieldValidator } from '../interfaces/IFieldValidator'
+import { IValueValidator } from '../interfaces/IValueValidator'
+import { JoinOptionsType } from '../types/JoinOptionsType'
+import { OrderByOptionsType } from '../types/OrderByOptionsType'
+import { PaginationOptionsType } from '../types/PaginationOptionsType'
+import { PartialFieldValueMapType } from '../types/PartialFieldValueMapType'
+import { WhereOptionsType } from '../types/WhereOptionsType'
 
 export const validateWhereOptions = <FieldsEnum extends string>(
   where: WhereOptionsType<FieldsEnum>[],
   fieldValidator: IFieldValidator,
-  operatorValidator: IOperatorValidator,
-  valueValidator: IValueValidator
+  valueValidator: IValueValidator,
 ): void => {
-  where.forEach(condition => {
+  where.forEach((condition) => {
     if (condition.field) {
-      fieldValidator.validateFields([condition.field]);
-    }
-    if (condition.operator) {
-      operatorValidator.validateOperators([condition.operator]);
+      fieldValidator.validateFields([condition.field])
     }
     if (condition.value !== undefined) {
-      valueValidator.validateValues([condition.value]);
+      valueValidator.validateValues([condition.value])
     }
-  });
-};
+  })
+}
 
 export const validateJoinOptions = (
   joinOptions: JoinOptionsType[],
-  fieldValidator: IFieldValidator
+  fieldValidator: IFieldValidator,
 ): void => {
-  joinOptions.forEach(join => {
-    if (join.type && !isValidJoinType(join.type)) {
-      throw new Error(`Invalid join type: ${join.type}`);
-    }
+  joinOptions.forEach((join) => {
     if (join.table) {
-      fieldValidator.validateFields([join.table]);
+      fieldValidator.validateFields([join.table])
     }
     Object.entries(join.on).forEach(([key, value]) => {
-      fieldValidator.validateFields([key, value]);
-    });
-  });
-};
+      fieldValidator.validateFields([key, value])
+    })
+  })
+}
 
 export const validatePagination = (pagination: PaginationOptionsType): void => {
   if (pagination.limit !== undefined && pagination.limit <= 0) {
-    throw new Error(`Invalid pagination limit: ${pagination.limit}`);
+    throw new Error(`Invalid pagination limit: ${pagination.limit}`)
   }
   if (pagination.offset !== undefined && pagination.offset < 0) {
-    throw new Error(`Invalid pagination offset: ${pagination.offset}`);
+    throw new Error(`Invalid pagination offset: ${pagination.offset}`)
   }
-};
+}
 
 export const validateOrderByOptions = <FieldsEnum extends string>(
   orderByOptions: OrderByOptionsType<FieldsEnum>[],
-  fieldValidator: IFieldValidator
+  fieldValidator: IFieldValidator,
 ): void => {
-  orderByOptions.forEach(orderBy => {
+  orderByOptions.forEach((orderBy) => {
     if (orderBy.field) {
-      fieldValidator.validateFields([orderBy.field]);
+      fieldValidator.validateFields([orderBy.field])
     }
-    if (orderBy.direction && !isValidOrderDirection(orderBy.direction)) {
-      throw new Error(`Invalid order by direction: ${orderBy.direction}`);
-    }
-  });
-};
+  })
+}
 
 export const validateData = <FieldsEnum extends string>(
   data: PartialFieldValueMapType<FieldsEnum>[],
   fieldValidator: IFieldValidator,
-  valueValidator: IValueValidator
+  valueValidator: IValueValidator,
 ): void => {
-  data.forEach(entry => {
+  data.forEach((entry) => {
     Object.entries(entry).forEach(([field, value]) => {
-      fieldValidator.validateFields([field as FieldsEnum]);
+      fieldValidator.validateFields([field as FieldsEnum])
       if (value !== undefined) {
-        valueValidator.validateValues([value]);
+        valueValidator.validateValues([value])
       }
-    });
-  });
-};
-
-const isValidJoinType = (joinType: string): joinType is JoinTypesEnum => {
-  return Object.values(joinTypes).includes(joinType as JoinTypesEnum);
-};
-
-const isValidOrderDirection = (direction: string): direction is OrderByTypesEnum => {
-  return Object.values(orderByTypes).includes(direction as OrderByTypesEnum);
-};
+    })
+  })
+}
