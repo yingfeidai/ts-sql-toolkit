590eb55eed624a4842a3c62ada6e604dfaba14a3
dyf96 <yingfei.dai@atrae.co.jp>
2024-08-22 16:31:48 +0900

    refactor: 🔨 use interface and fix lint

--- src/application/services/RepositoryService.ts
+++ src/application/services/RepositoryService.ts
@@ -1,86 +1,86 @@
-import { BatchInsertParams } from "../../domain/dtos/BatchInsertParams";
-import { DeleteParams } from "../../domain/dtos/DeleteParams";
+import { BatchInsertParams } from '../../domain/dtos/BatchInsertParams'
+import { DeleteParams } from '../../domain/dtos/DeleteParams'
 import {
   BatchExecutionErrorResult,
   ExecutionResult,
-} from "../../domain/dtos/ExecutionResult";
-import { InsertParams } from "../../domain/dtos/InsertParams";
-import { UpdateParams } from "../../domain/dtos/UpdateParams";
-import { operatorTypes } from "../../domain/enums/OperatorTypesEnum";
-import { SQLManager } from "../../domain/services/SQLManagerService";
-import { WhereOptionsType } from "../../domain/types/WhereOptionsType";
-import { Mapper } from "../../infrastructure/database/mappers/Mapper";
-import { FindAllParams } from "../dtos/FindAllParams";
-import { FindByIdParams } from "../dtos/FindByIdParams";
-import { FindManyParams } from "../dtos/FindManyParams";
-import { FindOneParams } from "../dtos/FindOneParams";
-import { ReplaceParams } from "../dtos/ReplaceParams";
-import { UpsertParams } from "../dtos/UpsertParams";
-import { IRepository } from "../interfaces/IRepository";
+} from '../../domain/dtos/ExecutionResult'
+import { InsertParams } from '../../domain/dtos/InsertParams'
+import { UpdateParams } from '../../domain/dtos/UpdateParams'
+import { operatorTypes } from '../../domain/enums/OperatorTypesEnum'
+import { WhereOptionsType } from '../../domain/types/WhereOptionsType'
+import { Mapper } from '../utils/mappers/Mapper'
+import { FindAllParams } from '../dtos/FindAllParams'
+import { FindByIdParams } from '../dtos/FindByIdParams'
+import { FindManyParams } from '../dtos/FindManyParams'
+import { FindOneParams } from '../dtos/FindOneParams'
+import { ReplaceParams } from '../dtos/ReplaceParams'
+import { UpsertParams } from '../dtos/UpsertParams'
+import { IRepository } from '../interfaces/IRepository'
+import { ISQLManager } from '../../domain/interfaces/ISQLManager'
+import { IMapper } from '../utils/mappers/IMapper'
 
 export class Repository<DbModel, FieldsEnum extends string, DomainEntity>
-  implements IRepository<FieldsEnum, DomainEntity>
-{
+  implements IRepository<FieldsEnum, DomainEntity> {
   constructor(
-    protected readonly sqlManager: SQLManager<FieldsEnum>,
+    protected readonly sqlManager: ISQLManager<FieldsEnum>,
     protected readonly tableName: string,
-    protected readonly mapper: Mapper<DbModel, DomainEntity>
+    protected readonly mapper: IMapper<DbModel, DomainEntity>,
   ) {}
 
   async findById(params: FindByIdParams): Promise<DomainEntity | null> {
     const results = await this.sqlManager.select(this.tableName, {
       where: [
         {
-          field: "id",
+          field: 'id',
           operator: operatorTypes.EQUALS,
           value: params.id,
         } as WhereOptionsType<FieldsEnum>,
       ],
-    });
+    })
     if (results.length === 0) {
-      return null;
+      return null
     }
-    return this.mapper.toDomainEntity(results[0]);
+    return this.mapper.toDomainEntity(results[0])
   }
 
   async findOne(
-    params: FindOneParams<FieldsEnum>
+    params: FindOneParams<FieldsEnum>,
   ): Promise<DomainEntity | null> {
-    const results = await this.sqlManager.select(this.tableName, { ...params });
+    const results = await this.sqlManager.select(this.tableName, { ...params })
     if (results.length === 0) {
-      return null;
+      return null
     }
-    return this.mapper.toDomainEntity(results[0]);
+    return this.mapper.toDomainEntity(results[0])
   }
 
   async findMany(params: FindManyParams<FieldsEnum>): Promise<DomainEntity[]> {
-    const results = await this.sqlManager.select(this.tableName, { ...params });
-    return results.map((result) => this.mapper.toDomainEntity(result));
+    const results = await this.sqlManager.select(this.tableName, { ...params })
+    return results.map((result: any) => this.mapper.toDomainEntity(result))
   }
 
   async findAll(params: FindAllParams<FieldsEnum>): Promise<DomainEntity[]> {
-    const results = await this.sqlManager.select(this.tableName, { ...params });
-    return results.map((result) => this.mapper.toDomainEntity(result));
+    const results = await this.sqlManager.select(this.tableName, { ...params })
+    return results.map((result: any) => this.mapper.toDomainEntity(result))
   }
 
   async createMany(params: InsertParams<FieldsEnum>): Promise<void> {
-    return this.sqlManager.insert(this.tableName, params);
+    return this.sqlManager.insert(this.tableName, params)
   }
 
   async updateMany(params: UpdateParams<FieldsEnum>): Promise<ExecutionResult> {
     return this.sqlManager.update(this.tableName, {
       ...params,
-    });
+    })
   }
 
   async deleteMany(params: DeleteParams<FieldsEnum>): Promise<ExecutionResult> {
-    return this.sqlManager.delete(this.tableName, { ...params });
+    return this.sqlManager.delete(this.tableName, { ...params })
   }
 
   async batchCreate(
-    params: BatchInsertParams<FieldsEnum>
+    params: BatchInsertParams<FieldsEnum>,
   ): Promise<BatchExecutionErrorResult<FieldsEnum>> {
-    return this.sqlManager.batchInsert(this.tableName, params);
+    return this.sqlManager.batchInsert(this.tableName, params)
   }
 
   async upsert(params: UpsertParams<FieldsEnum>): Promise<void> {
@@ -88,26 +88,26 @@ export class Repository<DbModel, FieldsEnum extends string, DomainEntity>
       async () => {
         const existingRecords = await this.sqlManager.select(this.tableName, {
           ...params.find,
-        });
+        })
         if (existingRecords.length > 0) {
           await this.sqlManager.update(this.tableName, {
             ...params.update,
-          });
+          })
         } else {
           await this.sqlManager.insert(this.tableName, {
             ...params.create,
-          });
+          })
         }
       },
-    ]);
+    ])
   }
 
   async replace(params: ReplaceParams<FieldsEnum>): Promise<void> {
     await this.sqlManager.executeTransaction([
       async () => {
-        await this.sqlManager.delete(this.tableName, params.delete);
-        await this.sqlManager.insert(this.tableName, params.create);
+        await this.sqlManager.delete(this.tableName, params.delete)
+        await this.sqlManager.insert(this.tableName, params.create)
       },
-    ]);
+    ])
   }
 }
